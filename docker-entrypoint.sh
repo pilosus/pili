7dd12091d1effee61cf63ecff13e034bfc052200
Vitaly R. Samigullin <vrs@pilosus.org>
2019-04-02 14:41:30 +0300

    Docker-compose env_file added, uwsgi config added

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Initialize DB..."
-python3.7 manage.py initialize
+if [[ ${FLASK_INIT} -eq 1 ]]
+then
+    echo "Initialize DB..."
+    python3.7 manage.py initialize
+fi
 
-echo "Apply migrations to DB..."
-python3.7 manage.py deploy
 
-echo "Run server..."
-python3.7 manage.py runserver --host=0.0.0.0
+if [[ ${FLASK_DEPLOY} -eq 1 ]]
+then
+    echo "Apply migrations to DB..."
+    python3.7 manage.py deploy
+fi
+
+
+if [[ ${FLASK_CONFIG} = "development" ]]
+then
+    echo "Run Flask development server..."
+    python3.7 manage.py runserver --host 0.0.0.0 --port 8080
+else
+    echo "Run uWSGI production server"
+    uwsgi --ini /app/etc/uwsgi.ini:production
+fi
