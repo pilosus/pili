5c6ed526c1c8a5558ee83995c3c29d540b19d732
Vitaly R. Samigullin <vrs@pilosus.org>
2019-04-02 15:44:08 +0300

    Makefile added

--- setup.py
+++ setup.py
@@ -11,11 +11,10 @@ def get_version() -> str:
     """ Return package version from git, write commit to file
     """
     tag = check_output(['git', 'describe', '--tags']).decode().strip().split('-')
-    commit = check_output(['git', 'rev-parse', '--short', 'HEAD']).\
-        decode().strip()
+    commit = check_output(['git', 'rev-parse', '--short', 'HEAD']).decode().strip()
 
     # Write commit SHA to file
-    with open(Status.COMMIT_FILE, 'w') as f:
+    with open(COMMIT_FILE, 'w') as f:
         f.write(commit)
 
     # Return package version
@@ -27,10 +26,11 @@ def get_version() -> str:
 setup(
     name='pili-cms',
     description='Blogging platform with social network features',
+    author='Vitaly R. Samigullin',
+    author_email='vrs@pilosus.org',
     url='https://github.com/pilosus/pili/',
     version=get_version(),
     python_requires='>=3.6',
-
     zip_safe=True,
     include_package_data=True,
     packages=find_packages(exclude=['tests']),
